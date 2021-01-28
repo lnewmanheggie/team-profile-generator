870899aa57d55c42cb402100b0bfd73c69daf6dd
lnewmanheggie <lnewmanheggie@gmail.com>
2021-01-27 21:43:45 -0600

    inquirer prompts working

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { managerQuestions, menu } = require('./questions')
+const { managerQuestions, menu, engineerQuestions, internQuestions } = require('./questions')
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+const employeeArr = [];
+
+const engineerFxn = () => {
+    inquirer  
+    .prompt(engineerQuestions)
+    .then(({engineerName, engineerId, engineerEmail, github}) => {
+        console.log(engineerName, engineerId, engineerEmail, github)
+        const en = new Engineer(engineerName, engineerId, engineerEmail, github);
+        employeeArr.push(en);
+        menuFunction();
+    })
+}
+
+const internFxn = () => {
+    inquirer  
+    .prompt(internQuestions)
+    .then(({internName, internId, internEmail, school}) => {
+        const intern = new Intern(internName, internId, internEmail, school);
+        employeeArr.push(intern);
+        menuFunction();
+    })
+}
 
 const menuFunction = () => {
     inquirer
-        .prompt(menu)
-        .then(({menuAnswer}) => {
-            switch (menuAnswer) {
-                case 'Add an engineer':
-                    console.log('engineer')
-                    break;
-                case 'Add an intern':
-                    console.log('intern');
-                    break;
-            }
-        })
+    .prompt(menu)
+    .then(({menuAnswer}) => {
+        switch (menuAnswer) {
+            case 'Add an engineer':
+                engineerFxn();
+                break;
+            case 'Add an intern':
+                internFxn();
+                break;
+            case 'Finish building my team':
+                console.log(employeeArr);
+                return;
+        }
+    })
 }
 
 inquirer 
     .prompt(managerQuestions)
     .then(({managerName, managerId, managerEmail, managerOffice}) => {
-        console.log(managerName, managerId, managerEmail, managerOffice)
+        const manager = new Manager(managerName, managerId, managerEmail, managerOffice);
+        employeeArr.push(manager);
         menuFunction();
     })
--- questions.js
+++ questions.js
@@ -30,4 +30,50 @@ const menu = [
     },
 ]
 
-module.exports = { managerQuestions, menu }
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "What is this engineer's name?",
+        name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: "What is their employee ID?",
+        name: 'engineerId',
+    },
+    {
+        type: 'input',
+        message: "What is their email?",
+        name: 'engineerEmail',
+    },
+    {
+        type: 'input',
+        message: "What is their github?",
+        name: 'github',
+    },
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: "What is this intern's name?",
+        name: 'internName',
+    },
+    {
+        type: 'input',
+        message: "What is the their employee ID?",
+        name: 'internId',
+    },
+    {
+        type: 'input',
+        message: "What is their email?",
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: "What is their school?",
+        name: 'school',
+    },
+]
+
+module.exports = { managerQuestions, menu, engineerQuestions, internQuestions}
